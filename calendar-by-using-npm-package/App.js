72dd7bfc407b687323904dc163c4e73f2ff7aa0f
Abhijeet Dattatray Mulik <90943636+adm1009@users.noreply.github.com>
2022-02-16 11:25:37 +0530

    Update App.js

--- calendar-by-using-npm-package/App.js
+++ calendar-by-using-npm-package/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
 import './App.css';
 function App() {
   const [value, onChange] = useState(new Date());
